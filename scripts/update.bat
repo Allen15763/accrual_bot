5312bbfde6dfd86b0b7426e9c2de3ae0bb21f8a8
Allen Li (李季倫) [MOTW1520] <lia@sea.com>
2026-03-29 13:37:34 +0800

    fix: credentials auto-copy 取代 env var、update 加 --no-deps

--- scripts/update.bat
+++ scripts/update.bat
@@ -13,9 +13,9 @@ echo.
 :: 判斷使用 venv 或 embedded
 if exist "%INSTALL_DIR%accrual_venv\Scripts\activate.bat" (
     call "%INSTALL_DIR%accrual_venv\Scripts\activate.bat"
-    pip install --upgrade "accrual-bot @ git+https://github.com/Allen15763/accrual_bot.git@packaging"
+    pip install --upgrade --force-reinstall --no-deps --no-cache-dir "accrual-bot @ git+https://github.com/Allen15763/accrual_bot.git@packaging"
 ) else if exist "%INSTALL_DIR%python311\python.exe" (
-    "%INSTALL_DIR%python311\python.exe" -m pip install --upgrade ^
+    "%INSTALL_DIR%python311\python.exe" -m pip install --upgrade --force-reinstall --no-deps --no-cache-dir ^
         "accrual-bot @ git+https://github.com/Allen15763/accrual_bot.git@packaging" ^
         --no-warn-script-location
 ) else (
